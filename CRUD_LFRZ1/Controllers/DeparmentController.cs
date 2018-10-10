8ac5da754580e2dd454115f9f86b9d526e1869be
lfrz74 <anonimus47@hotmail.com>
2018-10-10 09:30:56 -0500

    Primer commit de prueba

---
+++ CRUD_LFRZ1/Controllers/DeparmentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_LFRZ1.Models;
+
+namespace CRUD_LFRZ1.Controllers
+{
+    public class DeparmentController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: Deparment
+        public ActionResult Index()
+        {
+            return View(db.Deparments.ToList());
+        }
+
+        // GET: Deparment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deparment deparment = db.Deparments.Find(id);
+            if (deparment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deparment);
+        }
+
+        // GET: Deparment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deparment/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Deparment deparment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Deparments.Add(deparment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(deparment);
+        }
+
+        // GET: Deparment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deparment deparment = db.Deparments.Find(id);
+            if (deparment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deparment);
+        }
+
+        // POST: Deparment/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Deparment deparment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deparment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deparment);
+        }
+
+        // GET: Deparment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deparment deparment = db.Deparments.Find(id);
+            if (deparment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deparment);
+        }
+
+        // POST: Deparment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Deparment deparment = db.Deparments.Find(id);
+            db.Deparments.Remove(deparment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUD_LFRZ1/Controllers/EmployeeController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_LFRZ1.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace CRUD_LFRZ1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: Employee. Add PeagedListMVc in nugget
+        public ActionResult Index(string searchBy, string search, int? page, string sortBy)
+        {
+            
+            var employees = db.Employees.Include(e => e.Deparment).AsQueryable() ;
+
+            ViewBag.SortNameParameter = string.IsNullOrEmpty(sortBy) ? "Name desc" : "";
+            ViewBag.SortGenderParameter = sortBy == "Gender" ? "Gender desc" : "Gender";
+
+            
+            if (searchBy == "Gender")
+            {
+                employees = employees.Where(x => x.Gender == search || search == null);
+            }
+            else
+            {
+                employees = employees.Where(x => x.Name.StartsWith(search) || search == null);
+            }
+
+            switch (sortBy)
+            {
+                case "Name desc":
+                    employees = employees.OrderByDescending(x => x.Name);
+                    break;
+                case "Gender desc":
+                    employees = employees.OrderByDescending(x => x.Gender);
+                    break;
+                case "Gender":
+                    employees = employees.OrderBy(x => x.Gender);
+                    break;
+                default:
+                    employees = employees.OrderBy(x => x.Name);
+                    break; 
+            }
+            return View(employees.ToPagedList(page ?? 1, 2)); 
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Deparments, "Id", "Name");
+            return View();
+        }
+
+        // POST: Employee/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,Name,Gender,City,DepartmentId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Deparments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Deparments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,Name,Gender,City,DepartmentId")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Deparments, "Id", "Name", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
