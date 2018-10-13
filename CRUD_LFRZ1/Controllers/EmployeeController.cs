09368941c211eca1ae44750c41e584cd331a13f8
lfrz74 <anonimus47@hotmail.com>
2018-10-12 21:57:49 -0500

    Se quita el AntiForgeryToken para probar con Postman

--- CRUD_LFRZ1/Controllers/EmployeeController.cs
+++ CRUD_LFRZ1/Controllers/EmployeeController.cs
@@ -113,7 +113,7 @@ namespace CRUD_LFRZ1.Controllers
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "EmployeeId,Name,Gender,City,DepartmentId")] Employee employee)
         {
             if (ModelState.IsValid)
